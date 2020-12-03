828198a1c921cc8d2975b2b4ea44f1f7cc2b9e1a
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2020-12-03 12:00:19 -0300

    começando o server     modified:   Server.js 	new file:   code/img/Sol.svg 	new file:   code/img/chuva.svg 	new file:   code/img/nublado.svg 	new file:   code/img/nuvem.svg 	new file:   code/img/tempestade.svg 	modified:   code/index.html 	modified:   code/index.js 	new file:   code/list.html 	modified:   code/style/style.css 	modified:   package.json

--- Server.js
+++ Server.js
@@ -1,17 +1,22 @@
 const axios = require('axios')
-const cors = require ('cors')
+const cors = require('cors')
 const express = require('express')
+const BodyParser = require('body-parser')
 
 const key = '2616f7cb'
 
-let cidade = 'Guaxupe'
-let estado = 'MG'
+let cidade = 'new york'
+let estado = ''
 
 const app = express()
 
+app.use(BodyParser.urlencoded({ extend: false }))
+app.use(BodyParser.json())
+
 app.use(cors())
-app.get('/', async (req, res)=>{
-    try {   
+
+app.get('/', async (req, res) => {
+    try {
         const { data } = await axios(`https://api.hgbrasil.com/weather?key=${key}&city_name=${cidade},${estado}`)
         //console.log(data)
         //list()
@@ -24,4 +29,14 @@ app.get('/', async (req, res)=>{
     //return res.json(data)
 })
 
-app.listen('8000')
+app.get(`/View`, async (req, res) => {
+    let cidade = req.body.cidade
+    let estado = req.body.estado
+
+    console.log(cidade, estado)
+
+})
+
+app.listen(8000, () => {
+    console.log('rodando: http://localhost:8000/')
+})
--- code/index.js
+++ code/index.js
@@ -1,9 +1,9 @@
 async function MostarTempo() {
-    let cidade = document.querySelector("#cidade").value
-    let estado = document.querySelector("#estado").value
-    
+    //let cidade = document.querySelector("#cidade").value
+    //let estado = document.querySelector("#estado").value
+
     try {
-        const response = await fetch (`http://localhost:8000/`)
+        const response = await fetch(`http://localhost:8000/`)
         //console.log(response)
         const data = await response.json()
 
@@ -15,22 +15,86 @@ async function MostarTempo() {
 
 MostarTempo()
 
-function View(dias){
+function View(dias) {
+    let momentoAtual = new Date()
+    let json = momentoAtual.toLocaleDateString()
+    let string = json.toString()
+    string = string.split("/")
 
-     let list = ''
+    let day = string.shift()
+    let month = string.shift()
+    let comparation = `${day}/${month}`
+    // console.log(comparation)
+
+    let list = ''
+    let clima = []
+    let data = []
+    let description = []
+    let max = []
+    let min = []
+    let weekday = []
     //pega dados do users
-    for(let dia = 0; dia < 10; dia++){
+    for (let dia = 0; dia < 5; dia++) {
+        clima.push(dias.results.forecast[dia].condition)
+        data.push(dias.results.forecast[dia].date)
+        description.push(dias.results.forecast[dia].description)
+        max.push(dias.results.forecast[dia].max)
+        min.push(dias.results.forecast[dia].min)
+        weekday.push(dias.results.forecast[dia].weekday)
+        //console.log(clima, data, description, max, min, weekday)
+        
+        if(data[dia] == comparation){
+            list += `
+            <li class = 'card${dia}'>
+                <div class='data'>Hoje</div>
+                <div class='descricao'> ${description[dia]}  
+            `
+        }else{
+            list += `
+            <li class = 'card${dia}'>
+                <div class='data'>Dia: ${data[dia]}</div>
+                <div class='descricao'> ${description[dia]}    
+            `
+        }
+
+        if (clima[dia] == 'storm') {
+            list += `
+                <img src="./img/chuva.svg">
+            </div> 
+            `
+        }
+        if (clima[dia] == 'cloud') {
+            list += `
+                <img src="./img/nuvem.svg">
+            </div> 
+            `
+        }
+        if (clima[dia] == 'clear_day') {
+            list += `
+                <img src="./img/sol.svg">
+            </div>
+            `
+        }
+        if (clima[dia] === 'cloudly_day') {
+            list += `
+                <img src="./img/nublado.svg">
+            </div>
+            `
+        }
+        
         list += `
-            <li>
-                dia: ${dias.results.forecast[dia].date} <br>
-                condição: ${dias.results.forecast[dia].description}<br>
-                máxima: ${dias.results.forecast[dia].max}<br>
-                mínima: ${dias.results.forecast[dia].min}<br>
-                dia da semana: ${dias.results.forecast[dia].weekday}<br><br>
-            </li>
+            <div class="temp">
+                <span>Máxima: <span class='max'>${max[dia]} °C</span></span>
+                <span>Mínima: <span class='min'> ${min[dia]} °C</span></span>
+            </div>
+            <div class='dia'>Dia da semana: ${weekday[dia]}</div><br>
+        </li>
         `
     }
-    console.log(dias)
+    //console.log(dias)
 
-    document.querySelector('ul').innerHTML = `<h1>${dias.results.city}</h1>` + list
+    document.querySelector('ul').innerHTML = `
+    <h1>${dias.results.city}</h1>
+    <div class = 'card-deck m-0'>${list}</div>
+    `
 }
