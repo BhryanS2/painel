199fc26d5384ebe42e2fb1137fcb7b0b2512cafa
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-01-24 10:25:18 -0300

    arrumando server

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ async function MostarTempo() {
     let cidade =  document.querySelector("#cidade").value
     let estado =  document.querySelector("#estado").value
 
-    await fetch(`http://localhost:8000/weather/${cidade}/${estado}`)
+    await fetch(`https://server-tempo.herokuapp.com/weather/${cidade}/${estado}`)
     .then( async response => {
         const data = await response.json()
         View(data)
@@ -58,35 +58,35 @@ function View(dias) {
             `
         }
 
-        if (clima[dia] == 'storm') {
+        if (clima[dia] == 'storm' || clima[dia] == 'rain') {
             list += `
                 <img src="./img/chuva.svg">
             </div> 
             `
-        }
-        if (clima[dia] == 'cloud') {
+        }else if (clima[dia] == 'cloud') {
             list += `
                 <img src="./img/nuvem.svg">
             </div> 
             `
-        }
-        if (clima[dia] == 'clear_day') {
+        }else if (clima[dia] == 'clear_day') {
             list += `
                 <img src="./img/sol.svg">
             </div>
             `
-        }
-        if (clima[dia] === 'cloudly_day') {
+        }else if (clima[dia] === 'cloudly_day') {
             list += `
                 <img src="./img/nublado.svg">
             </div>
             `
+        } else {
+            list += `</div>`
         }
+
         
         list += `
             <div class="temp">
-                <span>Máxima: <span class='max'>${max[dia]} °C</span></span>
-                <span>Mínima: <span class='min'> ${min[dia]} °C</span></span>
+                <span>Máxima: <span class='max'>${max[dia]}°C</span></span>
+                <span>Mínima: <span class='min'> ${min[dia]}°C</span></span>
             </div>
             <div class='dia'>Dia da semana: ${weekday[dia]}</div><br>
         </li>
