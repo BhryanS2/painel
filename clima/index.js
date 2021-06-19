51e9c3bc98b85b3f46544fcab024b36e496fe96c
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-06-19 16:39:40 -0300

    melhorando o código

--- clima/index.js
+++ clima/index.js
@@ -1,156 +1,254 @@
-function getValues() {
-  const cidade = document.querySelector("#cidade").value
-  const estado = document.querySelector("#estado").value
-  return { cidade, estado }
-}
+const utils = {
+  getValues() {
+    cidade = document.querySelector("#cidade").value
+    const estado = document.querySelector("#estado").value
+    return { cidade, estado }
+  },
 
-function StrinfForURL(ArrayString) {
-  var stringFinal = ""
-  for (let letras in ArrayString) {
-    if (ArrayString[letras] == " ") ArrayString[letras] = "%20"
-    stringFinal += ArrayString[letras]
-  }
-  return stringFinal
-}
-
-function StringTreatment(str) {
-  str = str.replace(/[ÀÁÂÃÄÅ]/, "A")
-  str = str.replace(/[àáâãäå]/, "a")
-  str = str.replace(/[ÈÉÊË]/, "E")
-  str = str.replace(/[Ç]/, "C")
-  str = str.replace(/[ç]/, "c")
-  str = str.replace(/[ã]/, "a")
-  // o resto
-
-  return str
-}
-
-function ModfyString(cidade) {
-  let [...citystring] = StringTreatment(cidade)
-  return StrinfForURL(citystring)
-}
-
-async function Busca(cidade) {
-  const { estado } = getValues();
-  const result = await fetch(`https://server-tempo.herokuapp.com/weather/${cidade}/${estado}`)
-    .then(async response => {
-      return await response.json()
-    })
-    .catch(error => {
-      throw new Error('Não foi possivel fazer a busca')
-    })
-  return result
-}
-
-function setlaoding(loading) {
-  if (loading === true) {
-    let loadingEl = document.createElement('span')
-
-    loadingEl.appendChild(document.createTextNode('carregando...'))
-
-    loadingEl.setAttribute('id', 'loading')
-    const ul = document.querySelector('ul')
-    ul.appendChild(loadingEl)
-  }
-}
-
-function GetDateForComparation() {
-  let momentoAtual = new Date()
-  let json = momentoAtual.toLocaleDateString()
-  let string = json.toString()
-  string = string.split("/")
-
-  let day = string.shift()
-  let month = string.shift()
-  return `${day}/${month}`
-}
-
-function MostarTempo(dias, cityName, state) {
-  let comparation = GetDateForComparation()
-
-  let list = ""
-  let clima = []
-  let data = []
-  let description = []
-  let max = []
-  let min = []
-  let weekday = []
-  //pega dados do users
-  for (let dia = 0; dia < 5; dia++) {
-    clima.push(dias.results.forecast[dia].condition)
-    data.push(dias.results.forecast[dia].date)
-    description.push(dias.results.forecast[dia].description)
-    max.push(dias.results.forecast[dia].max)
-    min.push(dias.results.forecast[dia].min)
-    weekday.push(dias.results.forecast[dia].weekday)
-    //console.log(clima, data, description, max, min, weekday)
-
-    if (data[dia] == comparation) {
-      list += `
-        <li class = "card${dia}">
-          <div class="data">Hoje</div>
-          <div class="descricao"> ${description[dia]}`
-    } else {
-      list += `
-      <li class = "card${dia}">
-        <div class="data">Dia: ${data[dia]}</div>
-        <div class="descricao"> ${description[dia]}
-      `
+  StrinfForURL(ArrayString) {
+    var stringFinal = ""
+    for (let letras in ArrayString) {
+      if (ArrayString[letras] == " ") ArrayString[letras] = "%20"
+      stringFinal += ArrayString[letras]
     }
-    if (clima[dia] == "storm") {
-      list += `
-          <img src="./img/chuva.svg">
-      </div>
-      `
-    } else if (clima[dia] == "cloud") {
-      list += `
-          <img src="./img/nuvem.svg" class="cloud-IMG">
-      </div>
-      `
-    } else if (clima[dia] == "clear_day") {
-      list += `
-          <img src="./img/sol.svg">
-      </div>
-      `
-    } else if (clima[dia] === "cloudly_day") {
-      list += `
-          <img src="./img/nublado.svg">
-      </div>
-      `
-    } else if (clima[dia] == "rain") {
-      list += `
-          <img src="./img/tempestade.svg">
-      </div>
-      `
+    return stringFinal
+  },
+
+  StringTreatment(str) {
+    str = str.replace(/[ÀÁÂÃÄÅ]/, "A")
+    str = str.replace(/[àáâãäå]/, "a")
+    str = str.replace(/[ÈÉÊË]/, "E")
+    str = str.replace(/[Ç]/, "C")
+    str = str.replace(/[ç]/, "c")
+    str = str.replace(/[ã]/, "a")
+    // o resto
+
+    return str
+  },
+
+  ModfyString(cidade) {
+    let [...citystring] = this.StringTreatment(cidade)
+    return this.StrinfForURL(citystring)
+  },
+
+  GetDateForComparation() {
+    let momentoAtual = new Date()
+    let json = momentoAtual.toLocaleDateString()
+    let string = json.toString()
+    string = string.split("/")
+
+    let day = string.shift()
+    let month = string.shift()
+    return `${day}/${month}`
+  },
+
+  isToday(dia) {
+    let comparation = utils.GetDateForComparation()
+    return (dia == comparation)
+  },
+
+}
+
+const Weekdays = {
+  Dom() {
+    return "Domingo"
+  },
+  Seg() {
+    return "Segunda"
+  },
+  Ter() {
+    return "Terça"
+  },
+  Qua() {
+    return "Quarta"
+  },
+  Qui() {
+    return "Quinta"
+  },
+  Sex() {
+    return "Sexta"
+  },
+  Sab() {
+    return "Sábado"
+  }
+}
+
+const Search = {
+  isCityExist(cidade) {
+    //dias.results.forecast[dia].date
+    if (localStorage.getItem(cidade)) {
+      //é do mesmo dia?
+      const DataJson = JSON.parse(localStorage.getItem(cidade))
+      const day = DataJson.results.forecast[0].date;
+      return utils.isToday(day)
+
+    } else return false
+  },
+  async Busca(cidadeURL) {
+    const { estado, cidade } = utils.getValues();
+    if (this.isCityExist(cidade)) {
+      return JSON.parse(localStorage.getItem(cidade))
     } else {
-      list += `</div>`
+      const result = await fetch(`https://server-tempo.herokuapp.com/weather/${cidadeURL}/${estado}`)
+        .then(async response => {
+          const Res = await response.json()
+            localStorage.setItem(cidade, JSON.stringify(Res))
+            return Res
+        })
+        .catch(e => {
+          alert(e)
+          throw new Error('Não foi possivel fazer a busca')
+        })
+      return result
+    }
+  },
+}
+
+const setClimate = {
+  storm() {
+    return `<img src="./img/chuva.svg">`
+  },
+  cloud() {
+    return `<img src="./img/nuvem.svg" class="cloud-IMG" />`
+  },
+  clear_day() {
+    return `<img src="./img/sol.svg">`
+  },
+  cloudly_day() {
+    return `<img src="./img/nublado.svg">`
+  },
+  rain() {
+    return `<img src="./img/tempestade.svg">`
+  },
+}
+
+const RenderInScreen = {
+  setlaoding(loading) {
+    if (loading === true) {
+      let loadingEl = `<h1>carregando..., carregando...</h1>
+      <div class="card-deck m-0">
+          <li class="card0">
+              <div class="data">carregando...</div>
+              <div class="descricao">carregando...
+                  <img src="./img/Loading.svg">
+              </div>
+
+              <div class="temp">
+                  <span>Máxima: <span class="max">carregando...°C</span></span>
+                  <span>Mínima: <span class="min">carregando...C</span></span>
+              </div>
+              <div class="dia">Dia da semana: carregando...</div>
+          </li>
+          <li class="card1">
+              <div class="data">Dia:carregando...</div>
+              <div class="descricao">carregando...
+                  <img src="./img/Loading.svg">
+              </div>
+
+              <div class="temp">
+                  <span>Máxima: <span class="max">carregando...°C</span></span>
+                  <span>Mínima: <span class="min">carregando...C</span></span>
+              </div>
+              <div class="dia">Dia da semana: carregando...</div>
+          </li>
+          <li class="card2">
+              <div class="data">Dia:carregando...</div>
+              <div class="descricao">carregando...
+
+                  <img src="./img/Loading.svg">
+              </div>
+
+              <div class="temp">
+                  <span>Máxima: <span class="max">carregando...°C</span></span>
+                  <span>Mínima: <span class="min">carregando...C</span></span>
+              </div>
+              <div class="dia">Dia da semana: carregando...</div>
+          </li>
+          <li class="card3">
+              <div class="data">Dia:carregando...</div>
+              <div class="descricao"> Parcialmente nublado
+                  <img src="./img/Loading.svg">
+              </div>
+              <div class="temp">
+                  <span>Máxima: <span class="max">carregando...°C</span></span>
+                  <span>Mínima: <span class="min">carregando...C</span></span>
+              </div>
+              <div class="dia">Dia da semana: carregando...</div>
+          </li>
+          <li class="card4">
+              <div class="data">Dia:carregando...</div>
+              <div class="descricao"> Parcialmente nublado
+
+                  <img src="./img/Loading.svg">
+              </div>
+              <div class="temp">
+                  <span>Máxima: <span class="max">carregando...°C</span></span>
+                  <span>Mínima: <span class="min">carregando...C</span></span>
+              </div>
+              <div class="dia">Dia da semana: carregando......</div>
+          </li>
+      </div>
+    `
+      const ul = document.querySelector('ul')
+      ul.innerHTML = (loadingEl)
+    }
+  },
+
+  MostarTempo(dias, cityName, state) {
+
+    let list = ""
+    //pega dados do users
+    for (let dia = 0; dia < 5; dia++) {
+      const clima = dias.results.forecast[dia].condition
+      const data = dias.results.forecast[dia].date
+      const description = dias.results.forecast[dia].description
+      const max = dias.results.forecast[dia].max
+      const min = dias.results.forecast[dia].min
+      const weekday = utils.StringTreatment(dias.results.forecast[dia].weekday)
+      const istoday = utils.isToday(data)
+
+      list += `
+      <li class="card${dia}">
+        <div class="data">${istoday ? "Hoje" : data}</div>
+        <div class="descricao">
+          ${description}
+          ${setClimate[clima]()}
+        </div>
+        <div class="temp">
+          <span>Máxima:
+            <span class="max">${max}°C</span>
+          </span>
+          <span>Mínima:
+            <span class="min">${min}°C</span>
+          </span>
+        </div>
+        <div class="dia">Dia da semana: ${Weekdays[weekday]()}</div>
+      </li>`
     }
 
-    list += `
-      <div class="temp">
-        <span>Máxima: <span class="max">${max[dia]}°C</span></span>
-          <span>Mínima: <span class="min"> ${min[dia]}°C</span></span>
-      </div>
-      <div class="dia">Dia da semana: ${weekday[dia]}</div>
-    </li>`
-  }
-  //console.log(dias)
-
-  document.querySelector("ul").innerHTML = `
+    document.querySelector("ul").innerHTML = `
     <h1>${cityName}, ${state}</h1>
     <div class = "card-deck m-0">${list}</div>
     `
+  },
+
 }
 
-async function init() {
+
+async function init(cidade, estado) {
   try {
-    setlaoding(true)
-    const { cidade, estado } = getValues()
-    let cidadeURL = ModfyString(cidade)
-    const data = await Busca(cidadeURL)
-    MostarTempo(data, cidade, estado)
-    setlaoding(false)
+    RenderInScreen.setlaoding(true)
+    let cidadeURL = utils.ModfyString(cidade)
+    const data = await Search.Busca(cidadeURL)
+    RenderInScreen.MostarTempo(data, cidade, estado)
+    RenderInScreen.setlaoding(false)
   } catch (error) {
     alert(error.message)
   }
 }
+
+function reload() {
+  const { cidade, estado } = utils.getValues()
+  init(cidade, estado)
+}
+
